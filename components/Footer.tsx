6ea9bce3e4a688d65cf071425289d050eac56543
kimdolion <kimowilkes@gmail.com>
2023-01-31 12:50:20 -0500

    Update footer to include a linkedin icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,6 +7,7 @@ import { StyledLink } from "./StyledLink";
 
 import GithubIcon from './icons/GithubIcon';
 import vercel from '/public/vercel.svg'
+import LinkedInIcon from "./icons/LinkedInIcon";
 
 
 export const Footer = () => {
@@ -14,13 +15,10 @@ export const Footer = () => {
   
     return  (
       <footer className={`${styles.footer} ${theme}`}>
-        <StyledLink href="https://github.com/kimdolion/kimdolion.github.io" name="This Project's Github" leftIcon>
-          <GithubIcon width="1.5em" fill={theme === 'light' ? "": "white" } />
-        </StyledLink>
         <StyledLink href="https://github.com/kimdolion" name="My Github" leftIcon>
           <GithubIcon width="1.5em" fill={theme === 'light' ? "": "white" }  />
         </StyledLink>
-        <StyledLink href="https://linkedin.com/in/kimberly-wilkes" name="Linkedin" />
+        <StyledLink href="https://linkedin.com/in/kimberly-wilkes" name="Linkedin" leftIcon><LinkedInIcon fill={theme === 'light' ? "": "white" } width="1.5em" /></StyledLink>
         <Link
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           className={styles.styledLink}
