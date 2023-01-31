d595ac4a9f4a07d0769b1cb3c1d44d1bb5f7f816
kimdolion <kimowilkes@gmail.com>
2023-01-30 22:30:36 -0500

    Update provider to also give a isMobile context for simplicity

--- utils.tsx
+++ utils.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext, useEffect, useState } from "react"
+import { createContext, ReactNode, useContext, useEffect, useState, RefObject, Ref } from "react"
 import { randomFacts } from "./constants"
 
 export const getRandomFact = () => {
@@ -7,6 +7,25 @@ export const getRandomFact = () => {
     console.log(randomFacts[0])
     return (randomFacts[index])
 }
+
+
+export const useOutsideClick  = (ref: RefObject<HTMLDivElement>, handler: (event: MouseEvent | TouchEvent) => void ) => {
+
+  useEffect(() => {
+      const listener = (event: MouseEvent | TouchEvent) => {
+        if (!ref?.current || ref.current.contains(event.target as Node)) {
+            return;
+        }
+        handler(event);
+      };
+      document.addEventListener('mousedown', listener);
+      document.addEventListener('touchstart', listener)
+      return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener)
+      };
+  },[]);
+};
 
 
 // Light/Dark Mode https://github.com/nas5w/usecontext-theme-toggling
@@ -21,8 +40,13 @@ interface ThemeProviderProps {
   children: ReactNode
 }
 
+/**
+ * 
+ * @param children a ReactNode
+ * @returns Color Theme Provider that will set the color scheme to Light or Dark
+ */
 export const ThemeProvider = ({ children }: ThemeProviderProps) => {
-  const [theme, setTheme] = useState<Theme>("light");
+  const [theme, setTheme] = useState<Theme>("dark");
   
   
   // get the defined mode from the browser
@@ -43,3 +67,45 @@ export const ThemeProvider = ({ children }: ThemeProviderProps) => {
  };
 
  export const useTheme = () => useContext(ThemeContext)
+
+
+ 
+type Mobile = boolean;
+type MobileContext = { isMobile: Mobile}
+
+/**
+ * 
+ * @param children a ReactNode
+ * @returns Screen Sizing Provider that will return whether the screen size is within a mobile view size of 768px or smaller
+ */
+export const MobileContext = createContext<MobileContext>(
+    {} as MobileContext
+);
+
+interface MobileProviderProps {
+  children: ReactNode
+}
+
+export const MobileProvider = ({ children }: MobileProviderProps) => {
+  const [width, setWidth] = useState(1200)
+  const [isMobile, setIsMobile] = useState(false)
+  
+  useEffect(() => {
+    const handleResize = () => {
+        setWidth(window.innerWidth)
+        width <= 768 ? setIsMobile(true) : setIsMobile(false)
+    }
+    window.addEventListener('resize', handleResize)
+    handleResize()
+
+    return () => window.removeEventListener('resize', handleResize)
+}, [width])
+
+  return (
+    <MobileContext.Provider value={{ isMobile }}>
+      {children}
+    </MobileContext.Provider>
+  );
+ };
+
+ export const useMobile = () => useContext(MobileContext)
