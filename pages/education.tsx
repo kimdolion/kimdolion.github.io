c15c5a86068434b6eb277aa6128fddac43659e29
kimdolion <kimowilkes@gmail.com>
2023-02-06 14:13:30 -0500

    Update meta tags for other pages

--- pages/education.tsx
+++ pages/education.tsx
@@ -3,11 +3,20 @@ import { Page } from '@/components/Page'
 import { EducationSection } from '@/components/EducationSection'
 
 export default function Education() {
+  const title = "Kimberly Wilkes' Education"
+  const content = "Kimberly Wilkes' educational history; formal and informal."
+  const keywords = "Education, Software Engineer, Frontend Developer, Portfolio, Resume"
   return (
     <div>
       <Head>
-        <title>Kimberly Wilkes' Education</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content={title} />
+        <meta property="title" content={title} />
+        <meta property="og:description" content={content} />
+        <meta property="description" content={content} />
+        <meta property='og:keywords' content={keywords} />
+        <meta property='keywords' content={keywords} />
       </Head>
 
       <Page>
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,11 +8,20 @@ import { TableOfContents } from '@/components/TableOfContents'
 import { Divider } from '@/components/Divider'
 
 export default function Home() {
+  const title = "Kimberly Wilkes' Software Engineer Portfolio"
+  const content = "Kimberly Wilkes is a software engineer and former educator."
+  const keywords = "Software Engineer, Frontend Developer, Portfolio, Resume"
   return (
     <div>
       <Head>
-        <title>Kimberly Wilkes' Software Engineer Portfolio</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content={title} />
+        <meta property="title" content={title} />
+        <meta property="og:description" content={content} />
+        <meta property="description" content={content} />
+        <meta property='og:keywords' content={keywords} />
+        <meta property='keywords' content={keywords} />
       </Head>
 
       <Page>
